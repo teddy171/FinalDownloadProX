716a95455ab8ad57bbd1abc47177e7e1ea85d3de
teddy171 <teddy171@qq.com>
2022-07-26 13:16:47 +0800

    feat: Use external downloader aria2c

--- Final_Downloader/tasks.py
+++ Final_Downloader/tasks.py
@@ -4,7 +4,12 @@ import youtube_dl
 
 @shared_task
 def download_video(content, location):
-    ydl_opts = {"writeinfojson": True, "outtmpl":f"{location}/%(id)s/%(title)s.%(ext)s"}
+    ydl_opts = {
+        "writeinfojson": True, 
+        "outtmpl": f"{location}/%(id)s/%(title)s.%(ext)s", 
+        "external_downloader": "aria2c",
+        "external_downloader_args": ["-x 16", "-k 1M"]
+        }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([content])
 
