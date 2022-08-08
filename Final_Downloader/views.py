470f258fa798b86b575f774fbab66e01b6b544e5
teddy171 <teddy171@qq.com>
2022-08-08 14:16:24 +0800

    feat: use model Process for function download_satus, download_task and transmit_file

--- Final_Downloader/views.py
+++ Final_Downloader/views.py
@@ -4,7 +4,7 @@ import shutil
 import json
 from urllib.parse import urlparse
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponseRedirect
 from celery.result import AsyncResult
@@ -28,14 +28,12 @@ def clean_data(user):
         os.mkdir(f"data/{user}/search/")
     return f"data/{user}"
 
-def get_pure_filename(info_file, user_path, video_id):
+def get_pure_filename(user_path, video_id):
     files = os.listdir(f"{user_path}/{video_id}")
-    try:
-        files.remove(info_file)
-        files.remove("full_size.json")
-    except ValueError:
-        pass
-    return files[0]
+    if len(files != 1):
+        raise ValueError(f"More than a file {files}")
+    else:
+        return files[0]
 
 def download_video_info(content, location):
     ydl_opts = {
@@ -51,26 +49,6 @@ def save_form(form, owner):
         new_task = form.save(commit=False)
         new_task.owner = owner
         new_task.save()
-
-def get_download_satus(task_worker_dict, user_path):
-    for video_id, task_id in task_worker_dict.items():
-        async_res = AsyncResult(task_id)
-        status = async_res.state
-        files = os.listdir(f"{user_path}/{video_id}")
-        for file in files:
-            element = file.split('.')
-            if(element[-1] == 'json' and element[-2] == "info"):
-                info_file = file
-                files.remove(info_file)   
-        with open(f"{user_path}/{video_id}/{info_file}") as f:
-            info = json.load(f)
-            file_title = info["title"]
-            file_sizes = []
-            for format in info["formats"]:
-                if format["filesize"] != None:
-                    file_sizes.append(format["filesize"])
-            file_size = max(file_sizes)
-            yield (status, video_id, file_size, info_file, file_title)
 
 @login_required
 def new_task(request):
@@ -134,64 +112,51 @@ def download_task(request):
 @login_required
 def download_status(request):
     user_path = f"data/{request.user}"
-    try:
-        with open(f"{user_path}/work.json") as f:
-            task_worker = json.load(f)
-    except:
-        raise Http404
-    else:
-        tasks_status = list(get_download_satus(task_worker, user_path))
-        message = {}
-        for task_status in tasks_status:
-            status, video_id, file_size, info_file, video_title = task_status
-            if status == 'SUCCESS':
-                message[video_id] = {"status": status, "file_name": video_title}
+    aria2 = aria2p.API(
+        aria2p.Client(
+            host="http://localhost",
+            port=6800,
+            secret=""
+        )
+    )
 
-            elif status == "PENDING":
-                aria2 = aria2p.API(
-                    aria2p.Client(
-                        host="http://localhost",
-                        port=6800,
-                        secret=""
-                    )
-                )
+    processes = get_list_or_404(Process, owner=request.user)
 
-                while True:
-                    try:
-                        downloadings = aria2.get_downloads()
-                    except:
-                        pass
-                    else:
-                        break
-                file_curr_size = ""
-                for downloading in downloadings:
-                    if downloading.name.count(video_title) > 0:
-                        file_curr_size = "{:,}".format(downloading.progress)
-                if file_curr_size == "":
-                    file_curr_size = 0
-                file_size = "{:,}".format(file_size)
-                message[video_id] = {"status": status, "file_name": video_title, "file_curr_size": file_curr_size, "file_size" :file_size}
-        message = {"message": message}
-        return render(request, 'Final_Downloader/download_status.html', message)
+    message = {}
+    for process in processes:
+        # status, video_id, file_size, info_file, video_title = task_status
+        async_res = AsyncResult(process.task_id)
+        status = async_res.state
+        if status == 'SUCCESS':
+            message[process.video_id] = {"status": status, "file_name": process.video_name}
+
+        elif status == "PENDING":
+            downloadings = aria2.get_downloads()
+            file_curr_size = ""
+            for downloading in downloadings:
+                if downloading.name.count(process.video_name) > 0:
+                    file_curr_size = "{:,}".format(downloading.progress)
+            
+            message[process.video_id] = {
+                "status": status,
+                "file_name": process.video_name,
+                "file_curr_size": file_curr_size,
+                "file_size": f"{process.video_size:,}"
+            }
+    message = {"message": message}
+    return render(request, 'Final_Downloader/download_status.html', message)
 
 @login_required
 def transmit_file(request, video_id):
     user_path = f"data/{request.user}"
-    try:
-        with open(f"{user_path}/work.json") as f:
-            task_worker = json.load(f)
-    except FileNotFoundError:
+    process = get_object_or_404(Process, owner=request.user, video_id=video_id)
+    async_res = AsyncResult(process.task_id)
+    status = async_res.state
+    if status != 'SUCCESS':
         raise Http404
     else:
-        tasks_status = list(get_download_satus(task_worker, user_path))
-        for task_status in tasks_status:
-            status, video_id_tmp, _, info_file, file_title = task_status
-            if video_id == video_id_tmp:
-                if status != 'SUCCESS':
-                    raise Http404
-                else:
-                    pure_file_name = get_pure_filename(info_file, user_path, video_id)
-                    return HttpResponseRedirect(f"/{user_path}/{video_id}/{pure_file_name}")
+        pure_file_name = get_pure_filename(user_path, video_id)
+        return HttpResponseRedirect(f"/{user_path}/{video_id}/{pure_file_name}")
 
 @login_required
 def search_video(request, key_word):
