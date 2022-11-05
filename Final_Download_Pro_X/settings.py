6455cfe41af0ff93a646c5c911d0459ec7b2447f
teddy171 <teddy171@qq.com>
2022-11-05 11:26:09 +0800

    fix: remove django heroku

--- Final_Download_Pro_X/settings.py
+++ Final_Download_Pro_X/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import django_heroku
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,5 +143,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
