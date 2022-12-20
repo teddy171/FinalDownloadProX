5137ad5d3776f6a8745b0f43ffd1d0209064b545
teddy171 <teddy171@qq.com>
2022-12-20 13:47:10 +0800

    feat: Improve admin site

--- Final_Downloader/admin.py
+++ Final_Downloader/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 
 from .models import Task, Process, SearchResult
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('content', 'owner', )
+
+    list_per_page = 20
+
+    list_filter = ('content', 'owner', )
+
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('video_name', 'video_id', 'task_id', 'video_size')
+
+    list_per_page = 20
+
+    list_filter = ('video_name', 'video_id', 'task_id', 'video_size')
+
+class SearchResultAdmin(admin.ModelAdmin):
+    list_display = ('video_title', 'video_id', 'video_url', 'video_author')
+
+    list_per_page = 20
+
+    list_filter = ('video_title', 'video_id', 'video_url', 'video_author')
+
 admin.site.register(Task)
 admin.site.register(Process)
 admin.site.register(SearchResult)
